b81aea9da1fbd17fd017a137032f3f57ff1dd6b9
Maxim Unguryan <makswww1@gmail.com>
2022-10-16 14:18:35 +0400

    made header for application

--- src/App.js
+++ src/App.js
@@ -1,22 +1,12 @@
 import logo from './logo.svg';
 import './App.scss';
+import Island from './ui-kit/Island';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+    <div className="app">
+      <header className="header">
+        <img src={logo} className="logo" />
       </header>
     </div>
   );
