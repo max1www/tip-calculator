11e4f0263cdd598153745f26754c029c5b07fb3a
Maxim Unguryan <makswww1@gmail.com>
2022-10-17 20:11:02 +0400

    fixed positive integer

--- src/utils/RegEx.js
+++ src/utils/RegEx.js
@@ -1,3 +1,3 @@
-export const POSITIVE_INTEGER_REGEX = /^0$|^[1-9]*$/;
+export const POSITIVE_INTEGER_REGEX = /^0$|^[1-9][0-9]*?$/;
 export const LESS_THAN_100 = /^0$|^[1-9][0-9]?$|^100$/;
 export const POSITIVE_DECIMAL_REGEX = /^(?![.]0*$|0{2,}$|0\d+)\d+[.]?\d{0,2}$/;
